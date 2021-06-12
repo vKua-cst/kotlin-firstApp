360879cf12fae33f94e2a867894c2b5fd338897d
creusat.chris@gmail.com <creusat.chris@gmail.com>
2021-06-12 17:10:15 +0200

    Added styles, texts, fonts, colors. First try of the application

--- app/src/main/java/com/example/naturecollection/MainActivity.kt
+++ app/src/main/java/com/example/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Call fragment_container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
